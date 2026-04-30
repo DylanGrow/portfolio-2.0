37ea8ff19ea83ba27fe82bf473b0db7815091538
Skylar Grow <dylangrow@gmail.com>
2026-04-29 23:16:51 -0400

    Refactor service worker for asset caching

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,18 +1,24 @@
 const CACHE = "site-v1";
+
 const ASSETS = [
-  "/",
-  "/css/styles.css",
-  "/js/app.js"
+  "../",
+  "../index.html",
+  "../css/styles.css",
+  "../js/app.js",
+  "../js/reveal.js",
+  "../js/ui.js",
+  "../assets/icons/favicon.svg",
+  "../assets/images/placeholder.webp"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
+self.addEventListener("install", event => {
+  event.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
